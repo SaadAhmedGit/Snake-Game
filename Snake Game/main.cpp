9fc644716349db83b0b9e80600f0f417db87e18c
Saad Ahmed <65092551+SaadAhmedGit@users.noreply.github.com>
2022-04-22 11:49:32 +0500

    Added levels and different snake colors

--- Snake Game/main.cpp
+++ Snake Game/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <SFML/Graphics.hpp>
+#include <fstream>
 #include <Windows.h>
 #include <SFML/Audio.hpp>
 using namespace std;
@@ -49,17 +50,17 @@ struct Board
 	int W, H;
 };
 
-char Input();
-void DrawSnake(const Snake& S);
+char input();
+void draw_snake(const Player& P);
 void gotoRowCol(const Coord& pos);
-void initSnake(Player& P, const Board& B);
-void changeDirection(Player& P, const char c);
+void init_snake(Player& P, const Board& B);
+void change_direction(Player& P, const char c);
 //bool WallTouch(const Snake& S, const Board& B);
-bool AllSnakesAlive(const Player* Ps, const int pCount);
-void initPlayer(Player& P, const char* name, const Board& B);
-Player* initGame(Board& B, const int pCount, const char* name);
+bool all_snakes_alive(const Player* Ps, const int p_count);
+void init_player(Player& P, const char* name, const Board& B);
+Player* init_game(Board& B, const int p_count);
 
-void initSnake(Player& P, const Board& B) {
+void init_snake(Player& P, const Board& B) {
 	P.pSnake.len = 7;
 	P.pSnake.sym = char(178);
 	P.pSnake.headSym = char(153);
@@ -87,10 +88,8 @@ void initSnake(Player& P, const Board& B) {
 	}
 }
 
-void initPlayer(Player& P, const char* name, const Board& B) {
+void init_player(Player& P, const Board& B) {
 	P.score = 0;
-	P.name = name;
-
 	switch (P.pID) {
 	case 1:
 		P.kUP = 'W';
@@ -107,7 +106,7 @@ void initPlayer(Player& P, const char* name, const Board& B) {
 	default:
 		break;
 	}
-	initSnake(P, B);
+	init_snake(P, B);
 }
 void initBoard(Board& B) {
 	B.start.x = 5;
@@ -115,39 +114,31 @@ void initBoard(Board& B) {
 	B.W = 110;
 	B.H = 20;
 	B.bMap = new char[B.W * B.H]{};
-	//Add 's' meaning solid wall, at the borders of board
-	for (int i = 0; i < B.W; i++) {
-		B.bMap[i] = 's';
-		B.bMap[i + (B.H - 1) * B.W] = 's';
-	}
-	for (int i = 0; i < B.H; i++) {
-		B.bMap[i * B.W] = 's';
-		B.bMap[i * B.W + B.W - 1] = 's';
-	}
-	//Add 'p' chars in the shape of an empty 8x8 square near the middle
-	for (int i = B.start.x; i < B.start.x + 8; i++) {
-		for (int j = B.start.y; j < B.start.y + 8; j++) {
-			B.bMap[i + j * B.W] = 'p';
+	ifstream file("level-1.txt");
+	if (file.is_open()) {
+		for (int i = 0; i < B.H; i++) {
+			for (int j = 0; j < B.W; j++) {
+				file >> B.bMap[i * B.W + j];
+			}
 		}
+		file.close();
+	}
+	else {
+		cout << "Unable to open file";
 	}
 
 }
-
-Player* initGame(Board& B, const int pCount, const char* name) {
+Player* init_game(Board& B, const int p_count) {
 	initBoard(B);
-	Player* Ps = new Player[pCount]{};
-	for (int i = 0; i < pCount; i++) {
+	const auto Ps = new Player[p_count]{};
+	for (int i = 0; i < p_count; i++) {
 		Ps[i].pID = i + 1;
-		initPlayer(Ps[i], name, B);
+		init_player(Ps[i], B);
 	}
 	return Ps;
 }
-
-
-
-void DisplayBoard(const Board& B) {
+void display_board(const Board& B) {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
-	//Display B.bMap
 	for (int i = 0; i < B.H; i++) {
 		for (int j = 0; j < B.W; j++) {
 			switch (B.bMap[i * B.W + j]) {
@@ -167,29 +158,45 @@ void DisplayBoard(const Board& B) {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 }
 
-void DrawSnake(const Snake& P) {
+void draw_snake(const Player& P) {
+	int color = FOREGROUND_RED;
+	switch (P.pID) {
+	case 1:
+		color = FOREGROUND_RED;
+		break;
+	case 2:
+		color = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+		break;
+	default:
+		break;
+	}
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
-	gotoRowCol(P.body[0]);
-	cout << P.headSym;
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
-	for (int i = 1; i < P.len; i++) {
-		gotoRowCol(P.body[i]);
-		cout << P.sym;
+	gotoRowCol(P.pSnake.body[0]);
+	cout << P.pSnake.headSym;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+	for (int i = 1; i < P.pSnake.len; i++) {
+		gotoRowCol(P.pSnake.body[i]);
+		cout << P.pSnake.sym;
 	}
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 }
-void EraseSnake(const Snake& S) {
-	for (int i = 0; i < S.len; i++) {
+void erase_snake(const Snake& S, const Board& B) {
+	gotoRowCol(S.body[0]);
+	char c = B.bMap[((S.body[0].y - B.start.y) * B.W) + S.body[0].x - B.start.x];
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
+	cout << (c == 's' ? char(219) : ' ');
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	for (int i = 1; i < S.len; i++) {
 		gotoRowCol(S.body[i]);
 		cout << ' ';
 	}
 }
-void ShiftSnakeBody(const Snake& S) {
+void shift_snake_body(const Snake& S) {
 	for (int i = S.len - 1; i > 0; i--) {
 		S.body[i] = S.body[i - 1];
 	}
 }
-void MoveSnake(const Snake& S, const Board& B) {
+void move_snake(const Snake& S, const Board& B) {
 	switch (S.direction) {
 	case UP:
 		S.body[0].y--;
@@ -211,6 +218,7 @@ void MoveSnake(const Snake& S, const Board& B) {
 	}
 	else if (S.body[0].x > B.start.x + B.W - 1) {
 		S.body[0].x = B.start.x;
+		S.body[0].x = B.start.x;
 	}
 	if (S.body[0].y < B.start.y) {
 		S.body[0].y = B.start.y + B.H - 1;
@@ -220,9 +228,9 @@ void MoveSnake(const Snake& S, const Board& B) {
 	}
 }
 
-void GenerateFood(const Player* Ps, const Board& B, Food& f) {
+void generate_food(const Player* Ps, const Board& B, Food& f) {
 	if (!f.spawned && time(0) % 4 == 0) {
-		srand((u_int)time(0));
+		srand(static_cast<u_int>(time(nullptr)));  // NOLINT(cert-msc51-cpp)
 		f.fPos.x = (rand() % B.W) + B.start.x;
 		f.fPos.y = (rand() % B.H) + B.start.y;
 		f.spawned = true;
@@ -238,17 +246,20 @@ void GenerateFood(const Player* Ps, const Board& B, Food& f) {
 				}
 			}
 		}
+		if (B.bMap[f.fPos.y * B.W + f.fPos.x] != '.') {
+			f.spawned = false;
+		}
 	}
 }
 
-bool CheckFoodEaten(const Snake& S, Food& f) {
+bool check_food_eaten(const Snake& S, Food& f) {
 	if (S.body[0].x == f.fPos.x && S.body[0].y == f.fPos.y) {
 		f.spawned = false;
 		return true;
 	}
 	return false;
 }
-void DrawFood(Food& f) {
+void draw_food(Food& f) {
 	if (f.spawned && !f.drawn) {
 		gotoRowCol(f.fPos);
 		cout << f.fSym;
@@ -261,27 +272,27 @@ void DrawFood(Food& f) {
 	}
 }
 void ExpandSnake(Snake& S) {
-	const int newSize = S.len + 1;
-	Coord* temp = new Coord[newSize]{};
-	for (int i = 0; i < S.len && i < newSize; i++) {
+	const int new_size = S.len + 1;
+	const auto temp = new Coord[new_size]{};
+	for (int i = 0; i < S.len && i < new_size; i++) {
 		temp[i] = S.body[i];
 	}
 	switch (S.direction) {
 	case UP:
-		temp[newSize - 1].x = S.body[0].x;
-		temp[newSize - 1].y = S.body[0].y - 1;
+		temp[new_size - 1].x = S.body[0].x;
+		temp[new_size - 1].y = S.body[0].y - 1;
 		break;
 	case DOWN:
-		temp[newSize - 1].x = S.body[0].x;
-		temp[newSize - 1].y = S.body[0].y + 1;
+		temp[new_size - 1].x = S.body[0].x;
+		temp[new_size - 1].y = S.body[0].y + 1;
 		break;
 	case LEFT:
-		temp[newSize - 1].x = S.body[0].x - 1;
-		temp[newSize - 1].y = S.body[0].y;
+		temp[new_size - 1].x = S.body[0].x - 1;
+		temp[new_size - 1].y = S.body[0].y;
 		break;
 	case RIGHT:
-		temp[newSize - 1].x = S.body[0].x + 1;
-		temp[newSize - 1].y = S.body[0].y;
+		temp[new_size - 1].x = S.body[0].x + 1;
+		temp[new_size - 1].y = S.body[0].y;
 		break;
 	default:
 		break;
@@ -291,7 +302,7 @@ void ExpandSnake(Snake& S) {
 	S.len++;
 }
 
-bool CheckSnakeKilled(const Snake& S) {
+bool check_snake_killed(const Snake& S) {
 	for (int i = 1; i < S.len; i++) {
 		if (S.body[0].x == S.body[i].x && S.body[0].y == S.body[i].y) {
 			return true;
@@ -300,7 +311,7 @@ bool CheckSnakeKilled(const Snake& S) {
 	return false;
 }
 
-bool CheckSnakeTouch(const Player* Ps, const int mySnakeID, const int pCount) {
+bool check_snake_touch(const Player* Ps, const int mySnakeID, const int pCount) {
 	for (int i = 0; i < pCount; i++) {
 		if (i == mySnakeID || !Ps[i].alive) {
 			continue;
@@ -314,37 +325,37 @@ bool CheckSnakeTouch(const Player* Ps, const int mySnakeID, const int pCount) {
 	return false;
 }
 
-void KillSnake(Player& P) {
-	EraseSnake(P.pSnake);
+void kill_snake(Player& P, const Board& B) {
+	erase_snake(P.pSnake, B);
 	for (int i = 0; i < P.pSnake.len; i++) {
 		P.pSnake.body[i] = {};
 	}
 	P.alive = false;
 }
 
-void PlayMusic(sf::Music& msc, const char* fileName) {
+void play_music(sf::Music& msc, const char* fileName) {
 	if (!msc.openFromFile(fileName)) {
 		return;
 	}
 	msc.play();
 	msc.setLoop(true);
 }
-void PlaySound(sf::SoundBuffer& buffer, sf::Sound& snd, const char* fileName, const float vol, const float pitch) {
+void play_sound(sf::SoundBuffer& buffer, sf::Sound& snd, const char* fileName, const float vol, const float pitch) {
 	if (!buffer.loadFromFile(fileName)) return;
 	snd.setPitch(pitch);
 	snd.setVolume(vol);
 	snd.play();
 }
-void DisplayScores(const Player* Ps, const Board& B, const int pCount) {
+void display_scores(const Player* Ps, const Board& B, const int pCount) {
 	for (int i = 0; i < pCount; i++) {
 		gotoRowCol({ B.start.x, B.start.y + B.H + (i + 4) });
 		cout << "Player " << i + 1 << ": " << Ps[i].score;
 	}
 }
-char CheckWallHit(const Snake& S, const Board& B) {
-	return B.bMap[((S.body[0].y-B.start.y) * B.W) + S.body[0].x - B.start.x];
+char check_wall_hit(const Snake& S, const Board& B) {
+	return B.bMap[((S.body[0].y - B.start.y) * B.W) + S.body[0].x - B.start.x];
 }
-bool deductScore(Player& P) {
+bool deduct_score(Player& P) {
 	if (P.score - 10 >= 0) {
 		P.score -= 10;
 		return true;
@@ -357,6 +368,7 @@ bool deductScore(Player& P) {
 		return false;
 	}
 }
+
 int main() {
 	int pCount = 2;
 	Board B{};
@@ -364,64 +376,66 @@ int main() {
 	sf::SoundBuffer sfx;
 	sf::Sound sfx_snd(sfx);
 	f.fSym = '*';
-	Player* Ps = initGame(B, pCount, "Saad Ahmed");
+	Player* Ps = init_game(B, pCount);
 	gotoRowCol({ B.W / 2 - (35 / 2), B.H / 2 });
 	cout << "PRESS ANY KEY TO START THE GAME..."; (void)_getch();
 	system("cls");
-	DisplayBoard(B);
+	display_board(B);
 	sf::Music bg_music;
 	bg_music.setVolume(20);
-	PlayMusic(bg_music, "bg_music.wav");
-	while (AllSnakesAlive(Ps, pCount)) {
-		GenerateFood(Ps, B, f);
-		DisplayScores(Ps, B, pCount);
+	play_music(bg_music, "bg_music.wav");
+	while (all_snakes_alive(Ps, pCount)) {
+		generate_food(Ps, B, f);
+		display_scores(Ps, B, pCount);
 		for (int i = 0; i < pCount; i++) {
-			changeDirection(Ps[i], toupper(Input()));
+			change_direction(Ps[i], toupper(input()));
 			if (Ps[i].alive) {
-				EraseSnake(Ps[i].pSnake);
-				ShiftSnakeBody(Ps[i].pSnake);
-				MoveSnake(Ps[i].pSnake, B);
-				switch (CheckWallHit(Ps[i].pSnake, B)) {
+				erase_snake(Ps[i].pSnake, B);
+				shift_snake_body(Ps[i].pSnake);
+				move_snake(Ps[i].pSnake, B);
+				switch (check_wall_hit(Ps[i].pSnake, B)) {
 				case 's':
-					PlaySound(sfx, sfx_snd, "sfx_hit.wav", 20, 1.0f);
-					KillSnake(Ps[i]);
+					play_sound(sfx, sfx_snd, "sfx_hit.wav", 20, 1.0f);
+					kill_snake(Ps[i], B);
+					continue;
 					break;
 				case 'p':
-					PlaySound(sfx, sfx_snd, "sfx_hit.wav", 20, 1.0f);
-					if (!deductScore(Ps[i])) {
-						KillSnake(Ps[i]);
-						PlaySound(sfx, sfx_snd, "sfx_snake_death.wav", 40, 1);
+					play_sound(sfx, sfx_snd, "sfx_hit.wav", 20, 1.0f);
+					B.bMap[((Ps[i].pSnake.body[0].y - B.start.y) * B.W) + Ps[i].pSnake.body[0].x - B.start.x] = '.';
+					if (!deduct_score(Ps[i])) {
+						kill_snake(Ps[i], B);
+						play_sound(sfx, sfx_snd, "sfx_snake_death.wav", 40, 1);
 						continue;
 					}
 					break;
 				default:
 					break;
 				}
-				if (CheckSnakeKilled(Ps[i].pSnake)) {
-					KillSnake(Ps[i]);
-					PlaySound(sfx, sfx_snd, "sfx_snake_death.wav", 40, 1);
+				if (check_snake_killed(Ps[i].pSnake)) {
+					kill_snake(Ps[i], B);
+					play_sound(sfx, sfx_snd, "sfx_snake_death.wav", 40, 1);
 					continue;
 				}
-				else if (CheckSnakeTouch(Ps, i, pCount)) {
-					if (!deductScore(Ps[i])) {
-						KillSnake(Ps[i]);
-						PlaySound(sfx, sfx_snd, "sfx_snake_death.wav", 40, 1);
+				else if (check_snake_touch(Ps, i, pCount)) {
+					if (!deduct_score(Ps[i])) {
+						kill_snake(Ps[i], B);
+						play_sound(sfx, sfx_snd, "sfx_snake_death.wav", 40, 1);
 						continue;
 					}
 				}
-				else if (CheckFoodEaten(Ps[i].pSnake, f)) {
-					PlaySound(sfx, sfx_snd, "sfx-pop.wav", 40, 1);
+				else if (check_food_eaten(Ps[i].pSnake, f)) {
+					play_sound(sfx, sfx_snd, "sfx-pop.wav", 40, 1);
 					Ps[i].score += 10;
 					ExpandSnake(Ps[i].pSnake);
 				}
-				DrawSnake(Ps[i].pSnake);
+				draw_snake(Ps[i]);
 				FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
 			}
 		}
 		if (f.spawned && !f.drawn) {
-			PlaySound(sfx, sfx_snd, "sfx-pop.wav", 30, 5);
+			play_sound(sfx, sfx_snd, "sfx-pop.wav", 30, 5);
 		}
-		DrawFood(f);
+		draw_food(f);
 		Sleep(100);
 	}
 	bg_music.stop();
@@ -432,7 +446,7 @@ int main() {
 	return 0;
 
 }
-bool AllSnakesAlive(const Player* Ps, const int pCount) {
+bool all_snakes_alive(const Player* Ps, const int pCount) {
 	for (int i = 0; i < pCount; i++) {
 		if (Ps[i].alive) {
 			return true;
@@ -441,7 +455,7 @@ bool AllSnakesAlive(const Player* Ps, const int pCount) {
 	return false;
 }
 
-char Input() {
+char input() {
 	char c = 0;
 	if (_kbhit()) {
 		c = _getch();
@@ -452,15 +466,15 @@ char Input() {
 	return c;
 }
 
-void changeDirection(Player& P, const char c) {
-	if (c == P.kUP && P.pSnake.direction != DOWN)
-		P.pSnake.direction = UP;
-	else if (c == P.kDOWN && P.pSnake.direction != UP)
-		P.pSnake.direction = DOWN;
-	else if (c == P.kLEFT && P.pSnake.direction != RIGHT)
-		P.pSnake.direction = LEFT;
-	else if (c == P.kRIGHT && P.pSnake.direction != LEFT)
-		P.pSnake.direction = RIGHT;
+void change_direction(Player& p, const char c) {
+	if (c == p.kUP && p.pSnake.direction != DOWN)
+		p.pSnake.direction = UP;
+	else if (c == p.kDOWN && p.pSnake.direction != UP)
+		p.pSnake.direction = DOWN;
+	else if (c == p.kLEFT && p.pSnake.direction != RIGHT)
+		p.pSnake.direction = LEFT;
+	else if (c == p.kRIGHT && p.pSnake.direction != LEFT)
+		p.pSnake.direction = RIGHT;
 }
 
 void gotoRowCol(const Coord& pos) {
